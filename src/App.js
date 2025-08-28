e7d356c92b1e0e07597c4ffdffb6917c2fa1e7f0
Manjosh Lilhare <lilharemanjosh@gmail.com>
2025-08-28 09:43:04 +0530

    new update in master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './Views/HomePage';
+import HomePage from '../Views/HomePage';
 function App() {
   return (
     <div className="App">
